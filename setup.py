fed7f59962c900bf3124185e11f2da1504243a68
Rob Taylor <rob@synthesized.io>
2021-03-30 16:59:11 +0100

    add package files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+def get_version(path):
+    v = {}
+    with open(path) as f:
+        exec(f.read(), v)
+    return v["__version__"]
+
+
+setup(
+    version = get_version("fairness/version.py")
+)
