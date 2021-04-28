27c720964e29023c80385057008358d6bad28cfa
bogdansurdu <gs2318@ic.ac.uk>
2021-04-28 11:21:03 +0300

    correct small bugs and export functions

--- src/fairlens/sensitive/__init__.py
+++ src/fairlens/sensitive/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["detect_name", "detect_names", "detect_names_dict"]
+__all__ = ["detect_name", "detect_names", "detect_names_dict", "detect_names_dict_dataframe", "detect_names_dataframe"]
