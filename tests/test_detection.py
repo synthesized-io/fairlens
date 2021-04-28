b91e45bb5838783c3899d6b6f18ae244dab45b0c
bogdansurdu <gs2318@ic.ac.uk>
2021-04-28 11:22:00 +0300

    add initial tests for dataframe detection

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from fairlens.sensitive import detection as dt
 
 
@@ -17,3 +19,36 @@ def test_detect_names_dict():
     cols = ["age", "gender", "legality", "risk"]
     res = {"age": "Age", "gender": "Gender", "legality": None, "risk": None}
     assert dt.detect_names_dict(cols) == res
+
+
+def test_detect_names_dataframe_simple():
+    col_names = ["age", "sexual orientation", "salary", "score"]
+    df = pd.DataFrame(columns=col_names)
+    res = ["age", "sexual orientation"]
+    assert dt.detect_names_dataframe(df) == res
+
+
+def test_detect_names_dataframe_dict_simple():
+    col_names = ["native", "location", "house", "subscription", "salary", "religion", "score"]
+    df = pd.DataFrame(columns=col_names)
+    res = {
+        "native": "Nationality",
+        "location": "Nationality",
+        "house": "Family Status",
+        "religion": "Religion",
+        "salary": None,
+        "subscription": None,
+        "score": None,
+    }
+    assert dt.detect_names_dict_dataframe(df) == res
+
+
+def test_detect_names_dataframe_deep():
+    col_names = ["A", "B", "C", "Salary", "D", "Score"]
+    data = [
+        ["male", "hearing impairment", "heterosexual", "50000", "christianity", "10"],
+        ["female", "obsessive compulsive disorder", "asexual", "60000", "daoism", "10"],
+    ]
+    df = pd.DataFrame(data, columns=col_names)
+    res = ["A", "B", "C", "D"]
+    assert set(dt.detect_names_dataframe(df, deep_search=True)) == set(res)
