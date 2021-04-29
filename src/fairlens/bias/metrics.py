a185593b8b1217ac99e6a15de166eda3941b4bbe
hilly12 <aahilmehta12@gmail.com>
2021-04-29 12:05:36 +0100

    add, refactor metrics class imbalance, emd, ks distance, kl divergence js divergence

--- src/fairlens/bias/metrics.py
+++ src/fairlens/bias/metrics.py
@@ -1,113 +1,150 @@
-from typing import List
+from typing import Any, Dict, List, Optional, Tuple
 
 import numpy as np
 import pandas as pd
 from pyemd import emd as pemd
+from scipy.stats import entropy, ks_2samp
 
-# from scipy.stats import ks_2samp
-
-
-def compute_probabilities(df: pd.DataFrame, sensitive_attrs: List[str], target_attr: str) -> pd.DataFrame:
-    """Computes the probability distributions for all sensitive groups of the given sensitive attributes
-    with respect to the target attribute
-
-    Args:
-        df (pd.DataFrame):
-            The input dataframe.
-        sensitive_attrs (List[str]):
-            The sensitive attributes to be considered.
-        target_attr (str):
-            The target attribute with respect to which the probabilities will be computed.
-
-    Returns:
-        pd.DataFrame:
-            A dataframe containing the sensitive groups and their counts, probabilities
-    """
-
-    # Get group counts & rates
-    sensitive_group_target_counts = df.groupby(sensitive_attrs + [target_attr])[target_attr].aggregate(Count="count")
-    sensitive_group_size = df.groupby(sensitive_attrs).size()
-    sensitive_group_target_counts["Rate"] = sensitive_group_target_counts["Count"] / sensitive_group_size
-
-    # Get total counts & rates
-    target_totals = df.groupby(target_attr)[target_attr].aggregate(Count="count")
-    target_totals["Rate"] = target_totals / len(df)
-    target_totals = target_totals.set_index(pd.MultiIndex.from_tuples([("Total", a) for a in target_totals.index]))
-
-    return pd.concat((sensitive_group_target_counts, target_totals))
+from . import utils
 
 
-def emd(df: pd.DataFrame, sensitive_attrs: List[str], target_attr: str) -> pd.DataFrame:
-    """Computes the Earth Mover's Distance for all sensitive groups of the given sensitive attributes
-    with respect to the target attribute
+def class_imbalance(
+    df: pd.DataFrame, target_attr: str, group1: Dict[str, List[Any]], group2: Optional[Dict[str, List[Any]]] = None
+) -> float:
 
-    Args:
-        df (pd.DataFrame):
-            The input dataframe.
-        sensitive_attrs (List[str]):
-            The sensitive attributes to consider.
+    pred1, pred2 = utils.get_predicates(df, group1, group2)
 
-    Returns:
-        pd.DataFrame:
-            A dataframe containing the sensitive groups and their counts, Earth Mover's Distance.
-    """
-
-    df_count = compute_probabilities(df, sensitive_attrs, target_attr)
-
-    emd_dist = []
-    space = df_count.index.get_level_values(1).unique()
-
-    for sensitive_value in df_count.index.get_level_values(0).unique():
-        p_counts = df_count["Count"][sensitive_value].to_dict()
-
-        # Remove counts in current subsample
-        q_counts = {k: v - p_counts.get(k, 0) for k, v in df_count["Count"]["Total"].to_dict().items()}
-
-        p = np.array([float(p_counts[x]) if x in p_counts else 0.0 for x in space])
-        q = np.array([float(q_counts[x]) if x in q_counts else 0.0 for x in space])
-
-        p /= np.sum(p)
-        q /= np.sum(q)
-
-        distance_space = 1 - np.eye(len(space))
-
-        emd_dist.append(
-            {
-                df_count.index.names[0]: sensitive_value,
-                "Distance": pemd(p, q, distance_space),
-                "Count": df_count["Count"][sensitive_value].sum(),
-            }
-        )
-
-    return pd.DataFrame(emd_dist).set_index(df_count.index.names[0])
+    return (df[pred1].nunique() - df[pred2].nunique()) / df[target_attr].nunique()
 
 
-# def ks_distance(self, df: pd.DataFrame, sensitive_attr: List[str], alpha: float = 0.05) -> pd.DataFrame:
-#     # ignore rows which have nans in any of the given sensitive attrs
-#     groups = df.groupby(sensitive_attr).groups
-#     distances = []
-#     for sensitive_attr_values, idxs in groups.items():
-#         target_group = df.loc[df.index.isin(idxs), self.target]
-#         target_rest = df.loc[~df.index.isin(idxs), self.target]
-#         if len(target_group) == 0 or len(target_rest) == 0:
-#             continue
+def emd(
+    df: pd.DataFrame,
+    target_attr: str,
+    group1: Dict[str, List[Any]],
+    group2: Optional[Dict[str, List[Any]]] = None,
+    group1_counts: Optional[Dict[Any, int]] = None,
+    group2_counts: Optional[Dict[Any, int]] = None,
+) -> float:
 
-#         dist, pval = ks_2samp(target_group, target_rest)
-#         if pval < alpha:
-#             if np.mean(target_group) < df[self.target].mean():
-#                 dist = -dist
+    pred1, pred2 = utils.get_predicates(df, group1, group2)
 
-#             if isinstance(sensitive_attr_values, tuple) and len(sensitive_attr_values) > 1:
-#                 sensitive_attr_str = "({})".format(", ".join([str(sa) for sa in sensitive_attr_values]))
-#                 sensitive_attr_values = list(sensitive_attr_values)
-#             else:
-#                 sensitive_attr_str = sensitive_attr_values
-#                 sensitive_attr_values = [sensitive_attr_values]
+    group1_counts = group1_counts or df[pred1].groupby(target_attr)[target_attr].aggregate(Count="count").to_dict()
+    group2_counts = group2_counts or df[pred2].groupby(target_attr)[target_attr].aggregate(Count="count").to_dict()
 
-#             self.values_str_to_list[sensitive_attr_str] = sensitive_attr_values
-#             distances.append([sensitive_attr_str, len(idxs), dist])
+    space = df[target_attr].unique()
 
-#     name = sensitive_attr_concat_name(sensitive_attr)
-#     self.names_str_to_list[name] = sensitive_attr
+    p = np.zeros(len(space))
+    q = np.zeros(len(space))
+    for i, val in enumerate(space):
+        p[i] += group1_counts.get(val, 0)
+        q[i] += group2_counts.get(val, 0)
 
-#     return pd.DataFrame(distances, columns=[name, "Count", "Distance"]).set_index(name)
+    p /= p.sum()
+    q /= q.sum()
+
+    distance_space = 1 - np.eye(len(space))
+
+    return pemd(p, q, distance_space)
+
+
+def ks_distance(
+    df: pd.DataFrame,
+    target_attr: str,
+    group1: Dict[str, List[Any]],
+    group2: Optional[Dict[str, List[Any]]] = None,
+    alpha: float = 0.05,
+) -> Tuple[float, float]:
+
+    pred1, pred2 = utils.get_predicates(df, group1, group2)
+
+    return ks_2samp(df[pred1][target_attr], df[pred2][target_attr])
+
+
+def kl_divergence(
+    df: pd.DataFrame,
+    target_attr: str,
+    group1: Dict[str, List[Any]],
+    group2: Optional[Dict[str, List[Any]]] = None,
+    group1_counts: Optional[Dict[Any, int]] = None,
+    group2_counts: Optional[Dict[Any, int]] = None,
+) -> float:
+
+    pred1, pred2 = utils.get_predicates(df, group1, group2)
+
+    group1_counts = group1_counts or df[pred1].groupby(target_attr)[target_attr].aggregate(Count="count").to_dict()
+    group2_counts = group2_counts or df[pred2].groupby(target_attr)[target_attr].aggregate(Count="count").to_dict()
+
+    space = df[target_attr].unique()
+
+    p = np.zeros(len(space))
+    q = np.zeros(len(space))
+    for i, val in enumerate(space):
+        p[i] += group1_counts.get(val, 0)
+        q[i] += group2_counts.get(val, 0)
+
+    p /= p.sum()
+    q /= q.sum()
+
+    return entropy(p, q)
+
+
+def js_divergence(
+    df: pd.DataFrame,
+    target_attr: str,
+    group1: Dict[str, List[Any]],
+    group2: Optional[Dict[str, List[Any]]] = None,
+    group1_counts: Optional[Dict[Any, int]] = None,
+    group2_counts: Optional[Dict[Any, int]] = None,
+    total_counts: Optional[Dict[Any, int]] = None,
+) -> float:
+
+    pred1, pred2 = utils.get_predicates(df, group1, group2)
+
+    group1_counts = group1_counts or df[pred1].groupby(target_attr)[target_attr].aggregate(Count="count").to_dict()
+    group2_counts = group2_counts or df[pred2].groupby(target_attr)[target_attr].aggregate(Count="count").to_dict()
+    total_counts = total_counts or df.groupby(target_attr)[target_attr].aggregate(Count="count").to_dict()
+
+    space = df[target_attr].unique()
+
+    p = np.zeros(len(space))
+    q = np.zeros(len(space))
+    pq = np.zeros(len(space))
+    for i, val in enumerate(space):
+        p[i] += group1_counts.get(val, 0)
+        q[i] += group2_counts.get(val, 0)
+        pq[i] += total_counts.get(val, 0)
+
+    p /= p.sum()
+    q /= q.sum()
+    pq /= pq.sum()
+
+    return (entropy(p, pq) + entropy(q, pq)) / 2
+
+
+def lp_norm(
+    df: pd.DataFrame,
+    target_attr: str,
+    group1: Dict[str, List[Any]],
+    group2: Optional[Dict[str, List[Any]]] = None,
+    p=2,
+    group1_counts: Optional[Dict[Any, int]] = None,
+    group2_counts: Optional[Dict[Any, int]] = None,
+) -> float:
+
+    pred1, pred2 = utils.get_predicates(df, group1, group2)
+
+    group1_counts = group1_counts or df[pred1].groupby(target_attr)[target_attr].aggregate(Count="count").to_dict()
+    group2_counts = group2_counts or df[pred2].groupby(target_attr)[target_attr].aggregate(Count="count").to_dict()
+
+    space = df[target_attr].unique()
+
+    p = np.zeros(len(space))
+    q = np.zeros(len(space))
+    for i, val in enumerate(space):
+        p[i] += group1_counts.get(val, 0)
+        q[i] += group2_counts.get(val, 0)
+
+    p /= p.sum()
+    q /= q.sum()
+
+    return np.linalg.norm(p, ord=p)
