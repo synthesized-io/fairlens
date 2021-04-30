da498de943392864d2ab756bf05f4a94e8b214db
bogdansurdu <gs2318@ic.ac.uk>
2021-04-30 12:06:49 +0300

    add initialization for default value and synonym dict from JSON file

--- src/fairlens/sensitive/config.py
+++ src/fairlens/sensitive/config.py
@@ -1,7 +1,16 @@
 import json
 import os
+from typing import Dict, List
 
 dict_path = "./configs/config_engb.json"
+attr_synonym_dict: Dict[str, List[str]] = {}
+attr_value_dict: Dict[str, List[str]] = {}
+
+# @dataclass
+# class SensitiveAttribute:
+#     name: str
+#     synonyms: List[str]
+#     values: List[str]
 
 
 def default_dict():
@@ -12,7 +21,15 @@ def default_dict():
     config_dict = json.load(json_file)
     json_file.close()
 
-    print(config_dict)
+    syn_dict = dict()
+    val_dict = dict()
+
+    for key, value in config_dict.items():
+        syn_dict[key] = value["synonyms"]
+        val_dict[key] = value["values"]
+
+    print(syn_dict)
+    print(val_dict)
 
 
 if __name__ == "__main__":
