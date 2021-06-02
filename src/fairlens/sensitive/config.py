81678d7cd9ca2ad4885f7bd73d3cfae6c7d078d0
bogdansurdu <gs2318@ic.ac.uk>
2021-06-02 17:36:27 +0300

    remove global variables from config.py

--- src/fairlens/sensitive/config.py
+++ src/fairlens/sensitive/config.py
@@ -1,20 +1,14 @@
 import json
 import os
 import pathlib
-from typing import Dict, List, Union
+from typing import Any, Tuple, Union
 
 PROJ_DIR = os.path.dirname(os.path.abspath(__file__))
 DEFAULT_PATH = os.path.join(PROJ_DIR, "./configs/config_engb.json")
 
-attr_synonym_dict: Dict[str, List[str]] = {}
-attr_value_dict: Dict[str, List[str]] = {}
 
-
-def load_config(config_path: Union[str, pathlib.Path] = DEFAULT_PATH):
+def load_config(config_path: Union[str, pathlib.Path] = DEFAULT_PATH) -> Tuple[Any, Any]:
     with open(config_path) as json_file:
         config_dict = json.load(json_file)
 
-    global attr_synonym_dict
-    attr_synonym_dict = config_dict["synonyms"]
-    global attr_value_dict
-    attr_value_dict = config_dict["values"]
+    return config_dict["synonyms"], config_dict["values"]
