d2558ac3ed24389ef1f84918581d0e4faa7ff902
Hilly12 <aahilmehta12@gmail.com>
2021-08-06 17:42:16 +0100

    add ethical add sidebars

--- docs/conf.py
+++ docs/conf.py
@@ -97,6 +97,10 @@ html_theme_options = {
     ],
 }
 
+html_sidebars = {
+    "**": ["search-field.html", "sidebar-nav-bs.html", "sidebar-ethical-ads.html"]
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
