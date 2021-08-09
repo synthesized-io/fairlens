6567bbfbedd0ce59bdd124eb61b99cfd34b8959d
Hilly12 <aahilmehta12@gmail.com>
2021-08-09 17:12:18 +0100

    move ethical ads from sidebar to bottom of page

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ html_theme_options = {
     "google_analytics_id": "UA-130210493-1",
 }
 
-html_sidebars = {"**": ["search-field.html", "sidebar-nav-bs.html", "sidebar-ethical-ads.html"]}
+html_sidebars = {"**": ["search-field.html", "sidebar-nav-bs.html"]}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
