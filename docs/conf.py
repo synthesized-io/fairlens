1711974ad32f21a92b2b22c2804116260275dd79
Aahil Mehta <aahilmehta12@gmail.com>
2021-08-11 16:57:00 +0100

    Aesthetic tweaks for rtd (#97)
    * style version popup
    
    * fix precommit
    
    * move version to bottom right, pad brand
    
    * invert x labels in heatmaps
    
    * move ethical ads from sidebar to bottom of page
    
    * fix precommit
    
    * add version to navbar
    
    * move version to top left
    
    Co-authored-by: Rob Taylor <rob@synthesized.io>

--- docs/conf.py
+++ docs/conf.py
@@ -96,9 +96,10 @@ html_theme_options = {
         {"name": "Synthesized", "url": "https://synthesized.io"},
     ],
     "google_analytics_id": "UA-130210493-1",
+    "navbar_start": ["navbar-logo", "version.html"],
 }
 
-html_sidebars = {"**": ["search-field.html", "sidebar-nav-bs.html", "sidebar-ethical-ads.html"]}
+html_sidebars = {"**": ["search-field.html", "sidebar-nav-bs.html"]}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
