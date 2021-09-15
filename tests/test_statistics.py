55bac647093243f4388c7b53bb718acc857b2576
bogdansurdu <gs2318@ic.ac.uk>
2021-09-15 04:33:43 +0300

    increase statistical analysis tests coverage

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -173,3 +173,89 @@ def test_sensitive_analysis_target_numeric():
     )
     assert res["Means"][0] == 75.5
     assert res["Variances"][0] == 2202.7
+
+
+def test_sensitive_analysis_target_datetime():
+    data = [
+        [10, "Caucasian", "24/4/1999"],
+        [25, "African-American", "19/7/1997"],
+        [15, "Hispanic", "31/12/2001"],
+        [34, "Other", "20/2/1998"],
+        [35, "Caucasian", "2/3/2002"],
+        [56, "Hispanic", "6/6/1997"],
+        [80, "African-American", "4/5/2000"],
+        [100, "African-American", "3/1/1996"],
+        [134, "Caucasian", "24/4/1999"],
+        [21, "African-American", "19/7/1997"],
+        [14, "Hispanic", "31/12/2001"],
+        [98, "Other", "20/2/1998"],
+        [76, "Caucasian", "2/3/2002"],
+        [51, "Hispanic", "6/6/1997"],
+        [82, "African-American", "4/5/2000"],
+        [145, "African-American", "3/1/1996"],
+    ]
+    res = fls.sensitive_group_analysis(
+        pd.DataFrame(data=data, columns=["score", "race", "date"]),
+        target_attr="date",
+        groups=[{"race": ["African-American"]}],
+        categorical_mode="square",
+    )
+    assert res["Means"][0] == datetime.datetime(1997, 12, 28)
+
+
+def test_sensitive_analysis_target_cat_square():
+    data = [
+        [10, "Caucasian", "24/4/1999", "Rejected"],
+        [25, "African-American", "19/7/1997", "Accepted"],
+        [15, "Hispanic", "31/12/2001", "Accepted"],
+        [34, "Other", "20/2/1998", "Rejected"],
+        [35, "Caucasian", "2/3/2002", "Accepted"],
+        [56, "Hispanic", "6/6/1997", "Accepted"],
+        [80, "African-American", "4/5/2000", "Accepted"],
+        [100, "African-American", "3/1/1996", "Accepted"],
+        [134, "Caucasian", "24/4/1999", "Rejected"],
+        [21, "African-American", "19/7/1997", "Rejected"],
+        [14, "Hispanic", "31/12/2001", "Rejected"],
+        [98, "Other", "20/2/1998", "Rejected"],
+        [76, "Caucasian", "2/3/2002", "Accepted"],
+        [51, "Hispanic", "6/6/1997", "Accepted"],
+        [82, "African-American", "4/5/2000", "Rejected"],
+        [145, "African-American", "3/1/1996", "Accepted"],
+    ]
+    res = fls.sensitive_group_analysis(
+        pd.DataFrame(data=data, columns=["score", "race", "date", "status"]),
+        target_attr="status",
+        groups=[{"race": ["Caucasian", "Hispanic"]}],
+        categorical_mode="square",
+    )
+    assert res["Means"][0] == "Accepted"
+    assert res["Variances"][0] == 0.53125
+
+
+def test_sensitive_analysis_target_cat_entropy():
+    data = [
+        [10, "Caucasian", "24/4/1999", "Rejected"],
+        [25, "African-American", "19/7/1997", "Rejected"],
+        [15, "Hispanic", "31/12/2001", "Accepted"],
+        [34, "Other", "20/2/1998", "Rejected"],
+        [35, "Caucasian", "2/3/2002", "Accepted"],
+        [56, "Hispanic", "6/6/1997", "Accepted"],
+        [80, "African-American", "4/5/2000", "Accepted"],
+        [100, "African-American", "3/1/1996", "Rejected"],
+        [134, "Caucasian", "24/4/1999", "Rejected"],
+        [21, "African-American", "19/7/1997", "Rejected"],
+        [14, "Hispanic", "31/12/2001", "Rejected"],
+        [98, "Other", "20/2/1998", "Rejected"],
+        [76, "Caucasian", "2/3/2002", "Accepted"],
+        [51, "Hispanic", "6/6/1997", "Accepted"],
+        [82, "African-American", "4/5/2000", "Rejected"],
+        [145, "African-American", "3/1/1996", "Accepted"],
+    ]
+    res = fls.sensitive_group_analysis(
+        pd.DataFrame(data=data, columns=["score", "race", "date", "status"]),
+        target_attr="status",
+        groups=[{"race": ["African-American", "Other"]}],
+        categorical_mode="entropy",
+    )
+    assert res["Means"][0] == "Rejected"
+    assert res["Variances"][0] == pytest.approx(0.5623, rel=1e-3)
